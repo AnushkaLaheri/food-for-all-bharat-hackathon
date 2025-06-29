a5295689539b4e6f0fdb8a9c855d8478b6f00ecd
AnushkaLaheri <anushkalaheri@gmail.com>
2025-06-29 22:14:05 +0530

    added

--- backend/server.py
+++ backend/server.py
@@ -5,7 +5,7 @@ from flask_cors import CORS  # ✅ THIS IS MISSING
 
 app = create_app()
 
-CORS(app, resources={r"/api/*": {"origins": "https://food-for-all-bharat-hackathon.vercel.app"}})
+CORS(app, origins=["https://food-for-all-bharat-hackathon.vercel.app"])
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=int(os.environ.get("PORT", 5000)), debug=True)
 
